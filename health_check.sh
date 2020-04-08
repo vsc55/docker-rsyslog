1513dc4dfa0c6cfc9e680a8900678e7b595cfd42
Jaiver Pastor <jpastor@cerebelum.net>
2020-04-08 18:48:19 +0200

    Add docker-compose
    run_srv > replace ports
    run_srv > add waiting

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-netstat -an | grep 514 > /dev/null
+UDP_PORT=${SYSLOG_UDP_PORT:-514}
+TCP_PORT=${SYSLOG_TCP_PORT:-514}
+
+netstat -an | grep ${TCP_PORT} > /dev/null
 if [ 0 != $? ]; then
 	echo "NOT OK"
 	exit 1
--- run_srv.sh
+++ run_srv.sh
@@ -9,9 +9,14 @@
 # @license GPL 3.0
 #
 
-
 UDP_PORT=${SYSLOG_UDP_PORT:-514}
 TCP_PORT=${SYSLOG_TCP_PORT:-514}
+
+MYSQL_HOST=${MYSQL_HOST:-"localhost"}
+MYSQL_PORT=${MYSQL_PORT:-"3306"}
+MYSQL_DB=${MYSQL_DB:-"syslog"}
+MYSQL_USER=${MYSQL_USER:-"root"}
+MYSQL_PASS=${MYSQL_PASS:-""}
 
 EXEC_SRV=/usr/sbin/rsyslogd
 EXEC_SRV_ARGS=
@@ -21,35 +26,40 @@ PATH_DATA=/data
 PATH_CONF=$PATH_DATA/rsyslog.conf
 PATH_PID=/var/run/rsyslog.pid
 
-
-fun_waiting () {
-	RESULT=0
-
-	if [[ -f $PATH_PID ]]; then
-		pid=$(cat "$PATH_PID")
-		
-		for pid in $pids; do
-			#sleep 5
-			wait $pid || let "RESULT=1"
-		done
+fun_fix_port() {
+	if [[ -f "$PATH_CONF" ]]; then
+		sed -i "/input(type=\"imudp\"/c input(type=\"imudp\" port=\"${UDP_PORT}\")" "$PATH_CONF"
+		sed -i "/input(type=\"imtcp\"/c input(type=\"imtcp\" port=\"${TCP_PORT}\")" "$PATH_CONF"
 	fi
-
-	return $RESULT
 }
 
+fun_waiting () {
+	if [[ -f "$PATH_PID" ]]; then
+		pid=$(cat "$PATH_PID")
+		while [ -e /proc/$pid ]
+		do
+			sleep .6
+		done
+	fi
+}
 
-if [[ -f $EXEC_EXTERNAL ]]; then
+if [[ -f "$EXEC_EXTERNAL" ]]; then
 	echo "*** RUN EXTERNAL ***"
-	sh $EXEC_EXTERNAL
+	sh "$EXEC_EXTERNAL"
 else
 	echo "Starting service..."
 
-	EXEC_SRV_ARGS=-i $PATH_PID
+	fun_fix_port
 
-	if [[ -f $PATH_CONF ]]; then
-		EXEC_SRV_ARGS=$EXEC_SRV_ARGS -c $PATH_CONF
+	EXEC_SRV_ARGS="-i $PATH_PID
+	if [[ -f "$PATH_CONF" ]]; then
+		EXEC_SRV_ARGS="$EXEC_SRV_ARGS -f $PATH_CONF"
 	fi
 
+	# DEBUG
+	#echo "CMD [ $EXEC_SRV ]"
+	#echo "ARG [ $EXEC_SRV_ARGS ]"
+
 	$EXEC_SRV $EXEC_SRV_ARGS
-	exit fun_waiting
+	fun_waiting
 fi
