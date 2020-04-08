8cf2626634b4d92ae819579106a1982180a5f11f
Jaiver Pastor <jpastor@cerebelum.net>
2020-04-08 16:58:57 +0200

    Update run_srv

--- run_srv.sh
+++ run_srv.sh
@@ -10,13 +10,46 @@
 #
 
 
+UDP_PORT=${SYSLOG_UDP_PORT:-514}
+TCP_PORT=${SYSLOG_TCP_PORT:-514}
+
 EXEC_SRV=/usr/sbin/rsyslogd
+EXEC_SRV_ARGS=
 EXEC_EXTERNAL=/data/external.sh
+
+PATH_DATA=/data
+PATH_CONF=$PATH_DATA/rsyslog.conf
+PATH_PID=/var/run/rsyslog.pid
+
+
+fun_waiting () {
+	RESULT=0
+
+	if [[ -f $PATH_PID ]]; then
+		pid=$(cat "$PATH_PID")
+		
+		for pid in $pids; do
+			#sleep 5
+			wait $pid || let "RESULT=1"
+		done
+	fi
+
+	return $RESULT
+}
+
 
 if [[ -f $EXEC_EXTERNAL ]]; then
 	echo "*** RUN EXTERNAL ***"
 	sh $EXEC_EXTERNAL
 else
 	echo "Starting service..."
-	$EXEC_SRV
+
+	EXEC_SRV_ARGS=-i $PATH_PID
+
+	if [[ -f $PATH_CONF ]]; then
+		EXEC_SRV_ARGS=$EXEC_SRV_ARGS -c $PATH_CONF
+	fi
+
+	$EXEC_SRV $EXEC_SRV_ARGS
+	exit fun_waiting
 fi
